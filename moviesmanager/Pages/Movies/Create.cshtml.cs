f856e3a65e3a5cd27e54c8c326248bd5cfe9294e
Matías Creimerman <mecrei@hotmail.com>
2019-07-26 15:45:11 -0300

    Tutorial Step 3

---
+++ moviesmanager/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MoviesManager.Models;
+
+namespace MoviesManager.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MoviesManager.Models.MoviesManagerContext _context;
+
+        public CreateModel(MoviesManager.Models.MoviesManagerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ moviesmanager/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MoviesManager.Models;
+
+namespace MoviesManager.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MoviesManager.Models.MoviesManagerContext _context;
+
+        public IndexModel(MoviesManager.Models.MoviesManagerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
